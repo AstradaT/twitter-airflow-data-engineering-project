78c03bcd4924e05bb5c744e19a2639da9541b1d7
AstradaT <tomi_cuervocasla@hotmail.com>
2022-12-16 11:06:25 -0300

    Update etl function

--- twitter_etl.py
+++ twitter_etl.py
@@ -6,38 +6,31 @@ import s3fs
 
 def run_twitter_etl():
 
-    access_key = "" 
-    access_secret = "" 
-    consumer_key = ""
-    consumer_secret = ""
+    access_key = "KSxoWEQzPGbDwsdONq9kmAOsP" 
+    access_secret = "6jn7zvTmDAvWHEpHMi50jo8XEX7zPScWpBBv2xRLKsnx80y1bd" 
+    consumer_key = "1357174219-HOPhTQVgtfOThqOfWDN5fcvTSnzg0PbisZRbtaQ"
+    consumer_secret = "tZjKx72Yih9RJNFZnZhrbEHHqhNqkMsE2yHmJ9nbXzAHQ"
 
-
+    client = tweepy.Client("AAAAAAAAAAAAAAAAAAAAABLjkQEAAAAASmpjIBU04loRl22Rrb2fgoVViQQ%3Dx4qqvhMXqEwEryBHCGlCJpbrfivRLXYFbWiJA8VF0wz3WrGKui")
+    """
     # Twitter authentication
     auth = tweepy.OAuthHandler(access_key, access_secret)   
     auth.set_access_token(consumer_key, consumer_secret) 
 
     # # # Creating an API object 
     api = tweepy.API(auth)
-    tweets = api.user_timeline(screen_name='@elonmusk', 
+    """
+    tweets = client.get_users_tweets(id=client.get_user(username='TomiAstrada').data.id, 
                             # 200 is the maximum allowed count
-                            count=200,
-                            include_rts = False,
-                            # Necessary to keep full_text 
-                            # otherwise only the first 140 words are extracted
-                            tweet_mode = 'extended'
+                            max_results=10
                             )
-
+    
     list = []
-    for tweet in tweets:
-        text = tweet._json["full_text"]
-
-        refined_tweet = {"user": tweet.user.screen_name,
-                        'text' : text,
-                        'favorite_count' : tweet.favorite_count,
-                        'retweet_count' : tweet.retweet_count,
-                        'created_at' : tweet.created_at}
-        
-        list.append(refined_tweet)
+    for tweet in tweets.data:
+        list.append([tweet.id, tweet.text])
 
     df = pd.DataFrame(list)
-    df.to_csv('refined_tweets.csv')
+    df.to_csv('refined_tweets.csv', header=['id', 'text'])
+
+
+run_twitter_etl()
